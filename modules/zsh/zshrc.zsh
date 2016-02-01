540b9d622a8b6d694306e8e9b94bf820a1076f0e
Mickael Kerbrat <mickael.kerbrat@gmail.com>
2016-02-01 12:31:54 -0800

    added bash support

--- modules/zsh/zshrc.zsh
+++ modules/zsh/zshrc.zsh
@@ -22,11 +22,16 @@
 # Vim mode
 ############
 
-bindkey -v  # vim mode
-export KEYTIMEOUT=1  # timeout between the <ESC> in tenth of seconds
+if $is_zsh ; then
 
+  bindkey -v  # vim mode
+  export KEYTIMEOUT=1  # timeout between the <ESC> in tenth of seconds
 
+elif $is_bash ; then
 
+  set -o vi
+
+fi
 
 
 
