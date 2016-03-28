df06b60754db54f9f0c6023384bc49a5a7ecabc5
Mickael Kerbrat <mickael.kerbrat@gmail.com>
2016-03-28 10:40:20 -0700

    added some shortcuts

--- modules/zsh/aliases.zsh
+++ modules/zsh/aliases.zsh
@@ -7,6 +7,26 @@ alias q=exit
 alias d_reload!='. ~/.zshrc'
 alias d_update!='( cd ~/.dotfiles ; git pull )'
 
+alias o=open
+
+
+# remove the autocompletion, because it's a internal command
+# THe default completion beiing the files, which is what we want :)
+compdef -d r
+r(){
+  while [[ $# -gt 0 ]]; do
+    typeset f="$1" ; shift
+    if [[ -d "$f" ]]; then
+      rmdir -p -v -- "$f"
+    else
+      rm -v -- "$f"
+    fi
+  done
+}
+
+alias rr='rm -rf --'
+
+
 
 
 
